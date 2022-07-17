dfb1d080cd95a2f3cf81b2d5374d3ad3e3c64936
Gabriel Allegretti <gallegretti@inf.ufrgs.br>
2022-07-16 21:05:41 -0300

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +8,7 @@ export default defineConfig({
   base: './',
   build: {
     commonjsOptions: {
-      exclude: ['/node_modules/@coderline/**.js']
+      exclude: ['/node_modules/@coderline/**.js'],
     },
-  }
-})
+  },
+});
