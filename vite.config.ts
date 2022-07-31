c2072edeca0a68fc371a22d5c1e206a7f0a9fadc
Gabriel Allegretti <gallegretti@inf.ufrgs.br>
2022-07-31 11:26:05 -0300

    fix: vite not resolving paths correctly

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
   base: './',
   build: {
     commonjsOptions: {
