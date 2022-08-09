a24f91406ab3cfc1f9dc0eb90c02ec92202daa24
Gabriel Allegretti <gallegretti@inf.ufrgs.br>
2022-08-08 23:31:17 -0300

    fix: set text undo and redo

--- src/editor-actions/actions/set-text/set-text.ts
+++ src/editor-actions/actions/set-text/set-text.ts
@@ -5,8 +5,8 @@ class SetFretAction extends EditorActionInterface<EditorActionSetText> {
   do(action: EditorActionSetText): EditorActionResult {
     const didChange = action.data.beat.text !== action.data.text;
     // Store previous text so we can use it on the undo
-    action.data.previousText = action.data.beat.text ?? undefined;
-    action.data.beat.text = action.data.text;
+    action.data.previousText = action.data.beat.text ?? '';
+    action.data.beat.text = action.data.text === '' ? null : action.data.text;
     return {
       requiresRerender: didChange,
       requiresMidiUpdate: false,
@@ -14,11 +14,20 @@ class SetFretAction extends EditorActionInterface<EditorActionSetText> {
   }
 
   undo(action: EditorActionSetText): EditorActionResult {
-    action.data.beat.text = action.data.previousText ?? null;
-    return {
-      requiresRerender: true,
-      requiresMidiUpdate: false,
-    };
+    const { previousText, beat } = action.data;
+    if (previousText === undefined) {
+      return {
+        requiresMidiUpdate: false,
+        requiresRerender: false,
+      };
+    }
+    return this.do({
+      type: action.type,
+      data: {
+        text: previousText,
+        beat,
+      },
+    });
   }
 }
 
