550a69a5377c9b8de8b5dfbf67a7b401f6e690ed
Gabriel Allegretti <gallegretti@inf.ufrgs.br>
2022-08-18 12:22:44 -0300

    feat: replace chord glyph with SMuFL symbol

--- src/glyphs/beat/chord.tsx
+++ src/glyphs/beat/chord.tsx
@@ -17,11 +17,15 @@ export default function ChordGlyph(props: DynamicGlyphProps) {
         xmlns="http://www.w3.org/2000/svg"
         style={baseSvgStyle(props)}
       >
-        <path
-          stroke={color}
+        <text
           fill={color}
-          d="M 5 21 L 5 12 L 6 12 L 6 21 Z Z M 0 0 L 16 0 L 16 1 L 0 1 Z M 0 1 L 0 21 L 1 21 L 1 1 Z M 16 0 L 16 22 L 15 22 L 15 0 Z M 16 22 L 0 22 L 0 21 L 16 21 Z M 1 11 L 16 11 L 16 12 L 1 12 Z M 5 1 L 5 11 L 6 11 L 6 1 Z Z Z Z Z M 10 1 L 10 11 L 11 11 L 11 1 Z M 10 12 L 10 21 L 11 21 L 11 12 Z"
-        />
+          style={{ transform: 'translate(0px, 22px)', fontSize: '20px' }}
+        >
+          {
+            // https://w3c.github.io/smufl/latest/tables/chord-diagrams.html
+            '\uE854'
+          }
+        </text>
       </svg>
     </Tooltip>
   );
