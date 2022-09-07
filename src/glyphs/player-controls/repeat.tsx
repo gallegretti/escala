1719497467a03da5d20290d0e0763769fb806cb6
Gabriel Allegretti <gallegretti@inf.ufrgs.br>
2022-09-07 19:10:25 -0300

    change: repeat glyph so it looks better

--- src/glyphs/player-controls/repeat.tsx
+++ src/glyphs/player-controls/repeat.tsx
@@ -7,11 +7,12 @@ import baseSvgStyle from '../glyphBaseSvgStyle';
 
 export default function RepeatGlyph(props: DynamicGlyphProps) {
   const color = useGlyphColor(props);
+  // https://fonts.google.com/icons?selected=Material%20Icons%3Arepeat%3A
   return (
     <Tooltip title="Repeat">
       <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill={color} onClick={props.onClick} style={baseSvgStyle(props)}>
-        <path d="M0 0h24v24H0V0z" fill="none" />
-        <path d="M21 1H3c-1.1 0-2 .9-2 2v18c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V3c0-1.1-.9-2-2-2zM7 7h10v3l4-4-4-4v3H5v6h2V7zm10 10H7v-3l-4 4 4 4v-3h12v-6h-2v4z" fillRule="evenodd" />
+        <path d="M0 0h24v24H0z" fill="none" />
+        <path d="M7 7h10v3l4-4-4-4v3H5v6h2V7zm10 10H7v-3l-4 4 4 4v-3h12v-6h-2v4z" />
       </svg>
     </Tooltip>
   );
